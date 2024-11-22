e31e4f7f38e27470208bde4f358f43b556e1c07f
Jawad Makhoul <jawad.makhol@gmail.com>
2024-11-22 23:43:57 +0200

    Updating handle_message() function

--- Server_Setup.py
+++ Server_Setup.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
 
 
@@ -11,15 +11,18 @@ def sanity():return "Server is running"
 
 
 TOKEN = '7467453386:AAEPsIImeqVnwNfeARnSU_WGeqMVtbTqRXM'
-TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{TOKEN}/setWebhook?url=https://67f4-147-235-214-197.ngrok-free.app/message'.format(TOKEN)
+TELEGRAM_INIT_WEBHOOK_URL = f'https://api.telegram.org/bot{TOKEN}/setWebhook?url=https://67f4-147-235-214-197.ngrok-free.app/message'
+
 
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
 @app.route('/message', methods=["POST"])
 def handle_message():
     print("got message")
+    chat_id = request.get_json()['message']['chat']['id']
+    res = requests.get("https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={11}&text={got it}'"
+                       .format(TOKEN, chat_id, "Got it"))
     return Response("success")
-
 
 
 if __name__ == '__main__':
